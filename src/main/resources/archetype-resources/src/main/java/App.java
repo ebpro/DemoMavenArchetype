d7204dfa192b3d9f5c2f10dfb60b537e1b8f997e
Emmanuel Bruno <emmanuel.bruno@univ-tln.fr>
2020-12-10 14:57:42 +0100

    Initial release.

---
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $package;
+
+import lombok.extern.java.Log;
+
+/**
+ * Hello world!
+ */
+@Log
+public class App {
+    public static void main(String[] args) {
+        log.info("Hello World!");
+    }
+}
